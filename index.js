1919cf4b8565b3540f7fe41170a8b236d5a39395
subAngel <whoangel.agl@gmail.com>
2023-03-31 20:59:35 -0600

    Feat: creando enpoints de productos usando faker-js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('my-store:index');
+const { faker } = require('@faker-js/faker');
 
 const app = express();
 // * Settings
@@ -13,11 +14,34 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
 app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+app.get('/products/filter', (req, res) => {
+  res.send('yo soy un filter');
+});
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
     name: 'phone',
     price: 100,
+    id,
   });
+});
+app.get('/category/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({ categoryId, productId });
 });
 
 app.listen(app.get('port'), () => {

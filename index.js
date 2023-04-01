02d95eb7d34e9ddb7d67d628afa42bb2dfaebad9
subAngel <whoangel.agl@gmail.com>
2023-03-31 18:31:49 -0600

    Feat: implementando morgan y debug en el servidor

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-console.log('Hola mundo');
 const express = require('express');
+const morgan = require('morgan');
+const debug = require('debug')('my-store:index');
 
 const app = express();
-const port = 3000;
+// * Settings
+app.set('port', process.env.PORT || 3000);
+
+// * Middlewares
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
+app.listen(app.get('port'), () => {
+  debug(`server is listening on http://localhost:${app.get('port')}`);
 });

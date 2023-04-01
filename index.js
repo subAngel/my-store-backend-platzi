5a1a8843e71385356201e8158d87cd4080d79395
subAngel <whoangel.agl@gmail.com>
2023-03-31 18:19:43 -0600

    iniciando el servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Hola mundo');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});

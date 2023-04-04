cc8ea656923d1a3a336b831f87c8bb902e18917f
subAngel <whoangel.agl@gmail.com>
2023-04-04 00:20:28 -0600

    Feat: routing de categorias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const productsRouter = require("./products.routes");
-const usersRouter = require("./users.routes");
+// const usersRouter = require("./users.routes");
 const categoriesRouter = require("./categories.routes");
 
 function routerApi(app) {
@@ -9,7 +9,7 @@ function routerApi(app) {
 	app.use("/api/v1", router);
 	router.use("/products", productsRouter);
 	router.use("/categories", categoriesRouter);
-	router.use("/users", usersRouter);
+	// router.use("/users", usersRouter);
 }
 
 module.exports = routerApi;

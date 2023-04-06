6b486072f07f8788c552ee7787ff9c220c37c422
subAngel <whoangel.agl@gmail.com>
2023-04-05 21:29:20 -0600

    Model Product
    Agregando el modelo producto a setupsModels()

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,7 +1,9 @@
 const { User, UserSchema } = require("./user.model");
+const { Product, productSchema } = require("./product.model");
 
 function setupModels(sequelize) {
 	User.init(UserSchema, User.config(sequelize));
+	Product.init(productSchema, Product.config(sequelize));
 }
 
 module.exports = setupModels;

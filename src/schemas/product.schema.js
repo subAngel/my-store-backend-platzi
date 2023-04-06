5f3c4256464453e2daa451bd7db4b3da0d53c4b2
subAngel <whoangel.agl@gmail.com>
2023-04-06 17:56:49 -0600

    Fix: cambiando el id del esquema de productos para que no pida uuid

--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-const id = Joi.string().uuid();
+const id = Joi.string();
 const name = Joi.string().min(3).max(17);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();

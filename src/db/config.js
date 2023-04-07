59e7b2602d159909f3f7011c08abcb5ca525c4bc
subAngel <whoangel.agl@gmail.com>
2023-04-06 22:03:10 -0600

    Feat: creando configuracion para empezar a generar migraciones

---
+++ src/db/config.js
@@ -0,0 +1,18 @@
+const { config } = require("../config/config");
+
+const MOTOR_DB = "mysql";
+
+const USER = encodeURIComponent(config.db_user);
+const PASSWORD = encodeURIComponent(config.db_password);
+const URI = `${MOTOR_DB}://${USER}:${PASSWORD}@${config.db_host}:${config.db_port}/${config.db_name}`;
+
+module.exports = {
+	development: {
+		url: URI,
+		dialect: MOTOR_DB,
+	},
+	production: {
+		url: URI,
+		dialect: MOTOR_DB,
+	},
+};

5da9f78e3d719eb6807041730a26ef2249d3b75e
subAngel <whoangel.agl@gmail.com>
2023-04-06 22:31:47 -0600

    Feat: creando los comandos para correr las migraciones

--- src/db/migrations/20230407040758-create-user.js
+++ src/db/migrations/20230407040758-create-user.js
@@ -1,20 +1,20 @@
 "use strict";
 
 const { UserSchema, USER_TABLE } = require("../models/user.model");
-const { productSchema, PRODUCT_TABLE } = require("../models/product.model");
-const { CategorySchema, CATEGORY_TABLE } = require("../models/category.model");
+// const { productSchema, PRODUCT_TABLE } = require("../models/product.model");
+// const { CategorySchema, CATEGORY_TABLE } = require("../models/category.model");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface) {
 		await queryInterface.createTable(USER_TABLE, UserSchema);
-		await queryInterface.createTable(PRODUCT_TABLE, productSchema);
-		await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
+		// await queryInterface.createTable(PRODUCT_TABLE, productSchema);
+		// await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
 	},
 
 	async down(queryInterface) {
 		await queryInterface.dropTable(USER_TABLE);
-		await queryInterface.dropTable(PRODUCT_TABLE);
-		await queryInterface.dropTable(CATEGORY_TABLE);
+		// await queryInterface.dropTable(PRODUCT_TABLE);
+		// await queryInterface.dropTable(CATEGORY_TABLE);
 	},
 };

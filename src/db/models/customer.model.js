f93ba46d6a8862529e7070274c12941ff524914e
subAngel <whoangel.agl@gmail.com>
2023-04-07 21:05:42 -0600

    Feat: modelo del customer sin asociacion

--- src/db/models/customer.model.js
+++ src/db/models/customer.model.js
@@ -1,36 +1,25 @@
 const { Model, DataTypes, Sequelize } = require("sequelize");
 
-const USER_TABLE = "users";
+const CUSTOMER_TABLE = "customers";
 
-const UserSchema = {
+const CustomerSchema = {
 	id: {
 		allowNull: false,
 		autoIncrement: true,
 		primaryKey: true,
 		type: DataTypes.INTEGER,
 	},
-	// fullname: {
-	// 	allowNull: false,
-	// 	type: DataTypes.STRING,
-	// },
-	username: {
-		allowNull: false,
-		type: DataTypes.STRING,
-		unique: true,
-	},
-	password: {
+	name: {
 		allowNull: false,
 		type: DataTypes.STRING,
 	},
-	email: {
-		allowNull: false,
+	lastname: {
+		allowNull: true,
 		type: DataTypes.STRING,
-		unique: true,
 	},
-	role: {
+	phone: {
 		allowNull: false,
 		type: DataTypes.STRING,
-		defaultValue: "customer",
 	},
 	createdAt: {
 		allowNull: false,
@@ -40,7 +29,7 @@ const UserSchema = {
 	},
 };
 
-class User extends Model {
+class Customer extends Model {
 	static associate() {
 		// associations
 	}
@@ -48,11 +37,11 @@ class User extends Model {
 	static config(sequelize) {
 		return {
 			sequelize,
-			tableName: USER_TABLE,
-			modelName: "User",
+			tableName: CUSTOMER_TABLE,
+			modelName: "Customer",
 			timestamps: false,
 		};
 	}
 }
 
-module.exports = { USER_TABLE, UserSchema, User };
+module.exports = { CUSTOMER_TABLE, Customer, CustomerSchema };

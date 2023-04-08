b476786418c2c4ef7c46d5de78a41553128817c9
subAngel <whoangel.agl@gmail.com>
2023-04-07 22:14:20 -0600

    Refactor: Agregando los demas modelos al setupModels de la base de datos

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,11 +1,17 @@
 const { User, UserSchema } = require("./user.model");
 const { Product, productSchema } = require("./product.model");
 const { Category, CategorySchema } = require("./category.model");
+const { Customer, CustomerSchema } = require("./customer.model");
+const { Order, OrdersSchema } = require("./order.model");
+const { OrdersProduct, OrdersProductSchema } = require("./orders-products.model");
 
 function setupModels(sequelize) {
 	User.init(UserSchema, User.config(sequelize));
 	Product.init(productSchema, Product.config(sequelize));
 	Category.init(CategorySchema, Category.config(sequelize));
+	Customer.init(CustomerSchema, Customer.config(sequelize));
+	Order.init(OrdersSchema, Order.config(sequelize));
+	OrdersProduct.init(OrdersProductSchema, OrdersProduct.config(sequelize));
 }
 
 module.exports = setupModels;

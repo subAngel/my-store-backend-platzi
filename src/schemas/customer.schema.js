6d031b5c6351e0187ea150a87e8de2377b730f70
subAngel <whoangel.agl@gmail.com>
2023-04-08 22:37:48 -0600

    Feat: creando el usuario junto con el customer en el endpoint del customer

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -5,6 +5,13 @@ const name = Joi.string().min(3).max(30);
 const lastname = Joi.string();
 const phone = Joi.string();
 const userId = Joi.number().integer();
+// * user info
+const username = Joi.string()
+	.regex(/^[A-Za-z][A-Za-z0-9_]+$/)
+	.min(4)
+	.max(30);
+const password = Joi.string().alphanum().min(8);
+const email = Joi.string().email();
 
 const getCustomerSchema = Joi.object({
 	id: id.required(),
@@ -14,7 +21,11 @@ const createCustomerSchema = Joi.object({
 	name: name.required(),
 	lastname: lastname.required(),
 	phone: phone.required(),
-	userId: userId.required(),
+	user: Joi.object({
+		username: username.required(),
+		password: password.required(),
+		email: email.required(),
+	}),
 });
 
 const updateCustomerSchema = Joi.object({
--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -12,7 +12,11 @@ class CustomerService {
 	}
 
 	async create(data) {
-		const newCustomer = await models.Customer.create(data);
+		const newUser = await models.User.create(data.user);
+		const newCustomer = await models.Customer.create({
+			...data,
+			userId: newUser.id,
+		});
 		return newCustomer;
 	}
 

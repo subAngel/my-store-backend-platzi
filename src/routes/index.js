be0d7bf09e7dc4cc96766c1fdae429f02d0d33f1
subAngel <whoangel.agl@gmail.com>
2023-04-08 18:50:39 -0600

    Feat: Indexando las rutas de customers al servidor

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const productsRouter = require("./products.routes");
 const usersRouter = require("./users.routes");
 const categoriesRouter = require("./categories.routes");
+const customersRouter = require("./customers.routes");
 
 function routerApi(app) {
 	const router = express.Router();
@@ -10,6 +11,7 @@ function routerApi(app) {
 	router.use("/products", productsRouter);
 	router.use("/categories", categoriesRouter);
 	router.use("/users", usersRouter);
+	router.use("/customers", customersRouter);
 }
 
 module.exports = routerApi;

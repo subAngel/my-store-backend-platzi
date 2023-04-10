1c050cb870c9bbdd9da2857534c08eef9eb1e0b8
subAngel <whoangel.agl@gmail.com>
2023-04-09 19:45:24 -0600

    Feat: creando el esquema para validar los datos de la order con join

--- src/schemas/order.schema.js
+++ src/schemas/order.schema.js
@@ -2,4 +2,24 @@ const Joi = require("joi");
 
 const id = Joi.number().integer();
 const delivered = Joi.boolean();
-// const customer
+const customerId = Joi.number().integer();
+
+const createOrderSchema = Joi.object({
+	delivered,
+	customerId: customerId.required(),
+});
+
+const updateOrderSchema = Joi.object({
+	delivered,
+	customerId,
+});
+
+const getOrderSchema = Joi.object({
+	id: id.required(),
+});
+
+module.exports = {
+	createOrderSchema,
+	updateOrderSchema,
+	getOrderSchema,
+};

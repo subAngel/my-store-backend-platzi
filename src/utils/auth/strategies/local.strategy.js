e3cfe3bbaa64d61d32ad0ae6d56049573c76377e
subAngel <whoangel.agl@gmail.com>
2023-04-17 23:22:00 -0600

    fix(local.strategy): quitando la contraseña del response del login y agregando opciones de login

--- src/utils/auth/strategies/local.strategy.js
+++ src/utils/auth/strategies/local.strategy.js
@@ -4,20 +4,27 @@ const bcrypt = require("bcrypt");
 
 const UserService = require("../../../services/user.service");
 const service = new UserService();
-const LocalStrategy = new Strategy(async (username, password, done) => {
-	try {
-		const user = await service.findByUsername(username);
-		if (!user) {
-			done(boom.notFound(), false);
+const LocalStrategy = new Strategy(
+	{
+		usernameField: "username",
+		passwordField: "password",
+	},
+	async (username, password, done) => {
+		try {
+			const user = await service.findByUsername(username);
+			if (!user) {
+				done(boom.notFound(), false);
+			}
+			const isMatch = await bcrypt.compare(password, user.password);
+			if (!isMatch) {
+				done(boom.unauthorized(), false);
+			}
+			delete user.dataValues.password;
+			done(null, user);
+		} catch (error) {
+			done(error, false);
 		}
-		const isMatch = await bcrypt.compare(password, user.password);
-		if (!isMatch) {
-			done(boom.unauthorized(), false);
-		}
-		done(null, user);
-	} catch (error) {
-		done(error, false);
 	}
-});
+);
 
 module.exports = LocalStrategy;

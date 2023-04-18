3bd788b0c06abb7a65ad083d8c32eb318537421f
subAngel <whoangel.agl@gmail.com>
2023-04-18 11:00:40 -0600

    style(test-bcrypt): eliminando los archivo para encriptar y desencriptar

--- src/hash-pass.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function encriptar() {
-	const myPassword = "admin123";
-	const hash = await bcrypt.hash(myPassword, 10);
-	console.log(hash);
-}
-
-encriptar();

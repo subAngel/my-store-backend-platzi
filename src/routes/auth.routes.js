cec66435dd134493217d1c9eb34d32d7d398c576
subAngel <whoangel.agl@gmail.com>
2023-04-18 12:51:11 -0600

    refactor(auth.routes): regresando el token y el usuario al response del endpoint del login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const { config } = require("../config/config");
+
+const secret = config.secret_key;
 
 const router = express.Router();
 
@@ -9,7 +12,16 @@ router.post(
 	passport.authenticate("local", { session: false }),
 	async (req, res, next) => {
 		try {
-			res.json(req.user);
+			const user = req.user;
+			const payload = {
+				sub: user.id,
+				role: user.role,
+			};
+			const token = jwt.sign(payload, secret);
+			res.json({
+				user,
+				token,
+			});
 		} catch (error) {
 			next(error);
 		}

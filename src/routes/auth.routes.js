c2028506a5bb55b103f3383e6410a734ec0b1188
subAngel <whoangel.agl@gmail.com>
2023-04-25 00:19:09 -0600

    feat(auth.routes): endpoint para hacer un recovery de contraseñas
    Endpoint para enviar un correo para la recuperacion de la contraseña de un usuario

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
-const { config } = require("../config/config");
 const AuthService = require("../services/auth.service");
 
-const secret = config.secret_key;
 const service = new AuthService();
 
 const router = express.Router();
@@ -16,7 +13,6 @@ router.post(
 	async (req, res, next) => {
 		try {
 			const user = req.user;
-
 			res.json(service.signToken(user));
 		} catch (error) {
 			next(error);
@@ -27,6 +23,8 @@ router.post(
 router.post("/recovery", async (req, res, next) => {
 	try {
 		const { email } = req.body;
+		const rta = await service.sendMail(email);
+		res.json(rta);
 	} catch (error) {
 		next(error);
 	}
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 const boom = require("@hapi/boom");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 const UserService = require("./user.service");
 const { config } = require("../config/config");
@@ -13,7 +14,7 @@ class AuthService {
 			throw boom.notFound();
 		}
 		const isMatch = await bcrypt.compare(password, user.password);
-		if (isMatch) {
+		if (!isMatch) {
 			throw boom.unauthorized();
 		}
 		delete user.dataValues.password;
@@ -32,7 +33,33 @@ class AuthService {
 		};
 	}
 
-	sendMail(email) {}
+	async sendMail(email) {
+		const user = await service.findByEmail(email);
+		if (!user) {
+			// throw boom.notFound(`User with email '${email}' not found`,);
+			throw boom.unauthorized();
+		}
+		const transporter = nodemailer.createTransport({
+			host: "smtp.gmail.com",
+			port: 465,
+			secure: true,
+			auth: {
+				user: config.mail_user,
+				pass: config.mail_pass,
+			},
+		});
+		await transporter.sendMail({
+			from: "whoangel.agl@gmail.com", // * sender addres
+			to: `${user.email}`,
+			subject: "Recuperacion de contraseña",
+			text: "Recuperar contraseña",
+			html: "<b> hola mundo </b> ",
+		});
+
+		return {
+			message: "Mail sent",
+		};
+	}
 }
 
 module.exports = AuthService;
